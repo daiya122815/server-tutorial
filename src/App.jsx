c93ae1ce8451839b7afb7327a8488d756243965d
daiya122815 <daiya122815@gmail.com>
2024-11-18 20:58:34 +0900

    入力された名前をuseRefで一時保存

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,52 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function App() {
-    //   const [message, setMessage] = useState("");
+
     const [showStory, setShowStory] = useState(false);
+    const tmpName = useRef("");
+    const [customName, setCustomName] = useState("");
     const [randomXItem, setRandomXItem] = useState("");
     const [randomYItem, setRandomYItem] = useState("");
     const [randomZItem, setRandomZItem] = useState("");
+
     useEffect(() => {
         (async () => {
             const response = await fetch("/.netlify/functions/hello");
             const data = await response.json();
-            //   setMessage(data.message);
-            // setShowStory(data.showStory);
             setRandomXItem(data.randomXItem);
             setRandomYItem(data.randomYItem);
             setRandomZItem(data.randomZItem);
         })();
     }, []);
+
     return (
-        // <div>
-        //   <h1>{message}</h1>
-        // </div>
         <>
             <div>
                 <label htmlFor="customname">Enter custom name:</label>
-                <input type="text" placeholder="" />
+                <input
+                    type="text"
+                    placeholder=""
+                    onChange={(e) => tmpName.current=e.target.value}
+                />
             </div>
             <div>
-                {/* <label htmlFor="us">US</label>
-        <input type="radio" value="us" checked={ukus === "us"} />
-        <label htmlFor="uk">UK</label>
-        <input type="radio" value="uk" checked={ukus === "uk"} /> */}
+                {/* <label htmlFor="us">US</label> */}
+                {/* <input type="radio" value="us" checked={ukus === "us"} /> */}
+                {/* <label htmlFor="uk">UK</label> */}
+                {/* <input type="radio" value="uk" checked={ukus === "uk"} /> */}
             </div>
             <div>
-                {/* <button onClick={(e) => { handleClick }}>Generate random story</button> */}
-                {/* <button onClick={handleClick}>Generate random story</button> */}
                 <button
                     onClick={async (event) => {
                         event.preventDefault();
                         const response = await fetch("/.netlify/functions/hello");
                         const data = await response.json();
                         setShowStory(true);
+                        setCustomName(tmpName.current);
                         setRandomXItem(data.randomXItem);
                         setRandomYItem(data.randomYItem);
                         setRandomZItem(data.randomZItem);
-                        console.log(data);
+                        console.log(data, tmpName);
                     }}
                 >
                     Generate random story
@@ -54,7 +56,7 @@ export default function App() {
                 <p>
                     It was 94 fahrenheit outside, so {randomXItem} went for a walk. When they
                     got to {randomYItem}, they stared in horror for a few moments, then {randomZItem}.
-                    Bob saw the whole thing, but was not surprised — {randomXItem} weighs 300
+                    {customName} saw the whole thing, but was not surprised — {randomXItem} weighs 300
                     pounds, and it was a hot day.
                 </p>
             )}
